9b570e8b07f9b4a42639879e9ee28e80f3a20dbf
SmritiD2004 <smriti.dawane@gmail.com>
2025-08-13 10:20:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">My UI</div>
+    </>
+  )
+}
+
+export default App
